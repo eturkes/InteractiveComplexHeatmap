f71822e155524204a7e6b9bfebd38c8057842b67
Emir Turkes <eturkes@bu.edu>
2024-09-29 22:03:57 +0300

    Adjust hardcoded figure dimensions

--- R/shiny-server.R
+++ R/shiny-server.R
@@ -439,7 +439,7 @@ makeInteractiveComplexHeatmap = function(input, output, session, ht_list,
 		req(heatmap_initialized())
 
 		width = 1150
-	    height = 491
+	    height = 492
 
 		output[[qq("@{heatmap_id}_heatmap")]] = renderPlot({
 			
